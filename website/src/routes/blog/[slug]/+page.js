45c60a2ed21093fa407c526b206cb127075edf3d
zivoy <ziv132@gmail.com>
2023-06-28 01:17:37 -0400

    format

--- website/src/routes/blog/[slug]/+page.js
+++ website/src/routes/blog/[slug]/+page.js
@@ -1,25 +1,26 @@
-import {error} from '@sveltejs/kit';
-import {marked} from 'marked';
+import { error } from '@sveltejs/kit';
+import { marked } from 'marked';
 import sanitizeHtml from 'sanitize-html';
-import PocketBase from "pocketbase";
-import {PUBLIC_POCKETBASE_PAGEURL} from '$env/static/public';
+import PocketBase from 'pocketbase';
+import { PUBLIC_POCKETBASE_PAGEURL } from '$env/static/public';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({params}) {
-    const pb = new PocketBase(PUBLIC_POCKETBASE_PAGEURL)
-    try {
-        const data = await pb.collection('posts').getOne(params.slug, {
-            expand: 'creator,editors',
+export async function load({ params }) {
+  const pb = new PocketBase(PUBLIC_POCKETBASE_PAGEURL);
+  try {
+    const data = await pb.collection('posts').getOne(params.slug, {
+      expand: 'creator,editors'
+    });
+    return {
+      title: data.title,
+      content: sanitizeHtml(
+        marked.parse(data.content, {
+          headerIds: false,
+          mangle: false
         })
-        return {
-            title: data.title,
-            content: sanitizeHtml(marked.parse(data.content, {
-                    headerIds: false,
-                    mangle: false
-                })
-            )
-        };
-    } catch (e) {
-        throw error(404, 'Not found');
-    }
+      )
+    };
+  } catch (e) {
+    throw error(404, 'Not found');
+  }
 }
