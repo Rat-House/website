0cec172a87c56ea5c0afbefbebbcb80cb1ca3cbd
zivoy <ziv132@gmail.com>
2023-06-27 23:37:17 -0400

    cat errors

---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class="flex justify-center">
+<img src="https://http.cat/{$page.status}" alt="{$page.error.message}" class="max-h-[93vh] w-screen object-contain"/>
+</div>
