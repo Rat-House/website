a5580d6f1a390c7fda25d386f4333cdcea92e235
zivoy <ziv132@gmail.com>
2023-06-27 21:35:10 -0400

    shush linter

--- website/src/routes/login/+server.js
+++ website/src/routes/login/+server.js
@@ -7,6 +7,7 @@ export async function POST({ request, locals }) {
 	const { email, password } = await request.json();
 
 	const { token, record } = await locals.pb.collection('users').authWithPassword(email, password);
+	console.log('logged in', token, record);
 
 	return new Response('Success...');
 }
