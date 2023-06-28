d45d2fb79b31a374ab3014b95bfffdbd13d92156
zivoy <ziv132@gmail.com>
2023-06-27 23:58:25 -0400

    error

--- website/src/routes/+error.svelte
+++ website/src/routes/+error.svelte
@@ -5,7 +5,7 @@
 <div class="flex justify-center">
   <img
     src="https://http.cat/{$page.status}"
-    alt={$page.error.message}
+    alt={$page.error ? $page.error.message : 'error'}
     class="max-h-[93vh] w-screen object-contain"
   />
 </div>
