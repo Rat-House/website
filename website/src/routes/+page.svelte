356aacc992d75b22a059c96d256214d37219af15
ikeia <ike.moore@student.ocadu.ca>
2023-06-27 22:44:31 -0400

    Home page and theme switcher
    Work?

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,10 +1,14 @@
-<script>
-</script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="">
+      <h1 class="mx-auto text-5xl font-bold text-secondary leading-tight max-w-xl">Bridging Imagination and Technology</h1>
+      <p class="mx-auto py-6 leading normal max-w-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid velit quas, optio enim est blanditiis magnam tenetur eaque, eum, provident dolorem at neque hic error unde illum sit quibusdam obcaecati.</p>
+      <button class="btn btn-primary">Something?</button>
+    </div>
+  </div>
+</div>
 
 <style lang="postcss">
   :global(html) {
