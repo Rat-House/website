ecacf947085bc4e4b65b6f87f7af0f28d43c9dc8
ikeia <ike.moore@student.ocadu.ca>
2023-06-29 22:31:44 -0400

    A little bit of formatting

--- website/src/routes/blog/+page.svelte
+++ website/src/routes/blog/+page.svelte
@@ -15,21 +15,43 @@
   console.log(data.posts);
 </script>
 
-{#each data.posts.items as post}
-  <a href="/blog/{post.id}">
-    <div class="card card-compact w-96 bg-base-300 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">{post.title}</h2>
-        {#each post.tags as tagID}
-          {@const tag = getTag(post.expand.tags, tagID)}
-          <div class="badge">
-            <div class="tooltip" data-tip={tag.description}>
-              {tag.name}
+<h1 class="text-center text-6xl text-primary font-bold">Blog</h1>
+
+<div class="flex flex-col place-items-center justify-center m-8">
+  <div class="divider">Recent Posts</div>
+  {#each data.posts.items as post}
+    <a href="/blog/{post.id}">
+      <div class="card card-compact w-96 bg-base-300 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title text-secondary">{post.title}</h2>
+
+          <div class="flex flex-row">
+            <div class="avatar placeholder">
+              <div class="bg-neutral-focus text-neutral-content rounded-full w-8">
+                <span class="text-xs">AA</span>
+              </div>
+            </div>
+            <div class="flex flex-col mx-2 text-accent">
+              <p class="p-0 m-0">{post.expand.creator.name}</p>
+              <p class="p-0 m-0 text-xs">{post.created}</p>
             </div>
           </div>
-        {/each}
-        <p >{post.expand.creator.name}</p>
+          <div class="divider" />
+          <p class="truncate">{post.content}</p>
+          <div class="divider" />
+
+          <div class="flex flex-row">
+            {#each post.tags as tagID}
+              {@const tag = getTag(post.expand.tags, tagID)}
+              <div class="badge mx-1">
+                <div class="tooltip" data-tip={tag.description}>
+                  #{tag.name}
+                </div>
+              </div>
+            {/each}
+          </div>
+        </div>
       </div>
-    </div>
-  </a>
-{/each}
+    </a>
+  {/each}
+</div>
--- website/src/routes/blog/[slug]/+page.svelte
+++ website/src/routes/blog/[slug]/+page.svelte
@@ -11,8 +11,24 @@
   {#if !data.published}
     <p><i>unpublished</i></p>
   {/if}
-  <h1>{data.title}</h1>
-  <div class="markdown">
+  <h1 class="text-6xl text-center text-secondary mt-8 mb-4">{data.title}</h1>
+
+  <div class="flex flex-col place-items-center my-2">
+    <div class="flex flex-row place-items-center">
+      <div class="avatar placeholder">
+        <div class="bg-neutral-focus text-neutral-content rounded-full w-8">
+          <span class="text-xs">AA</span>
+        </div>
+      </div>
+      <div class="flex flex-col mx-2 text-accent">
+        <p class="p-0 m-0">{data.user.name}</p>
+        <p class="p-0 m-0 text-xs">{data.published}</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="divider" />
+  <div class="markdown my-8 mx-16">
     <!-- eslint-disable-next-line svelte/no-at-html-tags-->
     {@html data.content}
   </div>
