7c815b6e7fc6cd9fb7d11b578b62e798a0c1088f
zivoy <ziv132@gmail.com>
2023-06-29 20:14:29 -0400

    blog list

--- website/src/routes/blog/+page.js
+++ website/src/routes/blog/+page.js
@@ -1,9 +1,9 @@
-import { pb } from '$lib/pocketbase.js';
+import {pb} from '$lib/pocketbase.js';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ url }) {
+export async function load({url}) {
   let page = parseInt(url.searchParams.get('page')) || 1;
   return {
-    posts: pb.collection('posts').getList(page, 50, { filter: 'published=true' })
+    posts: pb.collection('posts').getList(page, 50, {filter: 'published=true', expand: 'creator,tags'}),
   };
 }
--- website/src/routes/blog/+page.svelte
+++ website/src/routes/blog/+page.svelte
@@ -1,6 +1,22 @@
 <script>
   export let data;
-  console.log(data);
 </script>
 
-{data}
+{#each data.posts.items as post}
+  <a href="/blog/{post.id}">
+  <div class="card card-compact w-96 bg-base-300 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title">{post.title}</h2>
+      {#each post.tags as tagID}
+        {@const tag = post.expand.tags.find(v => v.id === tagID)}
+        <div class="badge">
+          <div class="tooltip" data-tip="{tag.description}">
+            {tag.name}
+          </div>
+        </div>
+      {/each}
+      <p></p>
+    </div>
+  </div>
+  </a>
+{/each}
