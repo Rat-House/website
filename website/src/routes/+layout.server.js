533b51348a2c43975b9183c0fee9c1fdba8647f2
zivoy <ziv132@gmail.com>
2023-06-30 00:56:27 -0400

    fix not being able to view unpublished articles

--- website/src/routes/+layout.server.js
+++ website/src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ locals, cookies }) {
   return {
-    pbCookie: cookies.get('pb_auth') || '',
+    pbCookie: cookies.serialize('pb_auth', cookies.get('pb_auth') || ''),
     user: locals.pb && locals.pb.authStore.model ? locals.pb.authStore.model.export() : {},
     isLoggedIn: locals.pb ? locals.pb.authStore.isValid : false
   };
