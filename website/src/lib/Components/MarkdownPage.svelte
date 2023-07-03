a89cbc4817dadbbb3fc93817a6d1c0b3ff3dabba
zivoy <ziv132@gmail.com>
2023-07-02 20:09:54 -0400

    work with live update

--- website/src/lib/Components/MarkdownPage.svelte
+++ website/src/lib/Components/MarkdownPage.svelte
@@ -1,33 +1,62 @@
 <script>
   import './blogpost.pcss';
   import { renderMarkdown } from './markdownitParser.js';
-  import { onMount } from 'svelte';
+  import { afterUpdate, onMount } from 'svelte';
 
   export let text = '';
 
-  onMount(() => {
-    /** @type {Array.<HTMLLinkElement>} */
-    const users = Array.from(document.querySelectorAll('a.user'));
-    if (users.length !== 0) {
-      fetch(`/dynamic/userList?=${users.map((u) => `user=${u.innerText}`).join('&')}`).then(
-        async (r) => {
-          const results = await r.json();
-          for (let user of users) {
-            if (user.innerText in results) {
-              user.innerText = results[user.innerText].name;
-            } else {
-              const noUser = document.createElement('span');
-              noUser.innerHTML = user.innerHTML;
-              noUser.classList.add(...user.classList);
-              if (user.parentNode == null) continue;
-              user.parentNode.insertBefore(noUser, user);
-              user.parentNode.removeChild(user);
-            }
-          }
-        }
-      );
+  /** @type {Object.<string, import("../../dbtypes").User>} */
+  let users = {};
+  /** @type {Array.<string>} */
+  let notUsers = [];
+
+  /**
+   * @param {Array.<HTMLLinkElement>} userNodes
+   * @return {Promise<void>}
+   */
+  async function getNewUsers(userNodes) {
+    const userList = userNodes
+      .map((u) => u.href.replace(/^.*\//, ''))
+      .filter((u) => !(u in users || notUsers.includes(u)));
+    if (userList.length === 0) return;
+    console.log(notUsers);
+
+    const results = await fetch(`/dynamic/userList?${userList.map((u) => `user=${u}`).join('&')}`);
+    /** @type {Object.<string, import("../../dbtypes").User>} */
+    const json = await results.json();
+    for (const username of userList) {
+      if (username in json) users[username] = json[username];
+      else notUsers.push(username);
     }
-  });
+  }
+
+  /**
+   * @param {Array.<HTMLElement>} userNodes
+   */
+  function updateUserNodes(userNodes) {
+    for (let user of userNodes) {
+      if (user.innerText in users) {
+        user.innerText = users[user.innerText].name;
+      } else {
+        const noUser = document.createElement('span');
+        noUser.innerHTML = user.innerHTML;
+        noUser.classList.add(...user.classList);
+        if (user.parentNode == null) continue;
+        user.parentNode.insertBefore(noUser, user);
+        user.parentNode.removeChild(user);
+      }
+    }
+  }
+
+  async function updateUsers() {
+    /** @type {Array.<HTMLLinkElement>} */
+    const userNodes = Array.from(document.querySelectorAll('a.user'));
+    await getNewUsers(userNodes);
+    updateUserNodes(userNodes);
+  }
+
+  onMount(updateUsers);
+  afterUpdate(updateUsers);
 </script>
 
 <div class="markdown">
