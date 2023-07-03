87788853c4d34faf82c6d5115e9e14af9e47229b
zivoy <ziv132@gmail.com>
2023-07-02 20:10:06 -0400

    test

---
+++ website/src/routes/blog/new/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import MarkdownPage from '$lib/Components/MarkdownPage.svelte';
+  let text = '';
+</script>
+
+<textarea bind:value={text} cols="30" rows="10" />
+
+<MarkdownPage bind:text />
