8f7ac4ebb0e2aab84a0b28b6be36b68bdc24b0fe
zivoy <ziv132@gmail.com>
2023-07-04 22:07:05 -0400

    add error headers

--- website/src/routes/+error.svelte
+++ website/src/routes/+error.svelte
@@ -1,11 +1,24 @@
 <script>
   import { page } from '$app/stores';
+  import { HeaderBuilder } from '$lib/headers.js';
+  import { MetaTags } from 'svelte-meta-tags';
+
+  const errorMessage = $page.error ? $page.error.message : 'error';
+
+  const headers = new HeaderBuilder()
+    .setTitle(`Error ${$page.status}`)
+    .setDescription(errorMessage)
+    .updateUrl($page.url)
+    .setImage(`${$page.url.origin}/favicon-128.png`, 'website logo')
+    .export();
 </script>
+
+<MetaTags {...headers} />
 
 <div class="flex justify-center">
   <img
     src="https://http.cat/{$page.status}"
-    alt={$page.error ? $page.error.message : 'error'}
+    alt={errorMessage}
     class="max-h-[93vh] w-screen object-contain"
   />
 </div>
