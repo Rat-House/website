0ce786193859e413c6ebebf4afb6d30d99a90e45
zivoy <ziv132@gmail.com>
2023-07-06 03:27:22 -0400

    fix not being able to log in -- issue with side effects

--- website/src/routes/user/login/+page.js
+++ website/src/routes/user/login/+page.js
@@ -1,10 +1,14 @@
 import { redirect } from '@sveltejs/kit';
+import { browser } from '$app/environment';
+import { pb } from '$lib/pocketbase.js';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ data }) {
   if (data.authProviderRedirect !== '') {
     throw redirect(303, data.authProviderRedirect);
   }
+
+  if (!browser) pb.authStore.clear();
 
   return data;
 }
