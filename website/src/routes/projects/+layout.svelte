107e209f672d968981abcd2d8dfe20371f2d9824
zivoy <ziv132@gmail.com>
2023-07-06 02:59:07 -0400

    make simpler

--- website/src/routes/projects/+layout.svelte
+++ website/src/routes/projects/+layout.svelte
@@ -1,13 +1,8 @@
 <script>
   import { browser } from '$app/environment';
-
-  let js = false;
-  if (browser) {
-    js = true;
-  }
 </script>
 
-{#if js}
+{#if browser}
   <slot />
 {:else}
   this page requires js to work
