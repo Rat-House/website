acce3fafd1e78d6754f27b19d11fc42f8922de01
zivoy <ziv132@gmail.com>
2023-07-05 20:50:14 -0400

    linting

--- website/src/routes/contact/+page.server.js
+++ website/src/routes/contact/+page.server.js
@@ -3,17 +3,17 @@ import { fail } from '@sveltejs/kit';
 /** @type {import("./$types").Actions} */
 export const actions = {
   post: async ({ request, locals }) => {
-    const user = locals.pb.authStore.isValid ? locals.pb.authStore.model.id : '';
+    const user = locals.pb.authStore.model ? locals.pb.authStore.model.id : '';
     let formData = await request.formData();
 
     try {
       const record = await sendContactMessage(
         locals.pb,
         user,
-        formData.get('name') ?? '',
-        formData.get('email') ?? '',
-        formData.get('message') ?? '',
-        formData.get('reason') ?? ''
+        (formData.get('name') ?? '').toString(),
+        (formData.get('email') ?? '').toString(),
+        (formData.get('message') ?? '').toString(),
+        (formData.get('reason') ?? '').toString()
       );
 
       return {
@@ -25,12 +25,21 @@ export const actions = {
         email: formData.get('email'),
         reason: formData.get('reason'),
         message: formData.get('message'),
-        error: e.message
+        error: /** @type {Error} */ (e).message
       });
     }
   }
 };
 
+/**
+ * @param {import("../../dbtypes.d").PocketBase} pb
+ * @param {string} user
+ * @param {string} name
+ * @param {string} email
+ * @param {string} message
+ * @param {string} reason
+ * @return {Promise<import("pocketbase").Record>}
+ */
 async function sendContactMessage(pb, user, name, email, message, reason) {
   if (name === '') throw new Error('Name must be provided for contact');
   if (email === '') throw new Error('An email must be provided for contact');
--- website/src/routes/contact/+page.svelte
+++ website/src/routes/contact/+page.svelte
@@ -1,22 +1,25 @@
 <script>
-  import { enhance } from "$app/forms";
+  import { enhance } from '$app/forms';
 
   export let form;
   export let data;
 
   let sending = false;
 
-  let email, reason, name, message;
+  let /** @type {string} */ email,
+    /** @type {string} */ reason,
+    /** @type {string} */ name,
+    /** @type {string} */ message;
 
   function initFields() {
-    email = form?.email ?? "";
-    name = form?.name ?? "";
-    reason = form?.reason || "";
-    message = form?.message ?? "";
+    email = (form?.email ?? '').toString();
+    name = (form?.name ?? '').toString();
+    reason = (form?.reason ?? '').toString();
+    message = (form?.message ?? '').toString();
 
     if (data.isLoggedIn && form?.error === undefined) {
-      email = data.user.email ?? "";
-      name = data.user.name ?? "";
+      email = data.user?.email ?? '';
+      name = data.user?.name ?? '';
     }
   }
 
@@ -42,54 +45,52 @@
   method="POST"
   action="?/post"
   class="mx-auto md:w-2/3 xl:w-96"
-  use:enhance={()=>{
+  use:enhance={() => {
     sending = true;
-    return async ({ update })=>{
+    return async ({ update }) => {
       await update();
       sending = false;
       initFields();
-    }
+    };
   }}
 >
   <div class="form-control">
     <label class="label" for="name">
       <span class="label-text">Name</span>
     </label>
-    <input class="input input-bordered w-full"
-           id="name"
-           name="name"
-           type="text"
-           value={name}
-           required
-           disabled={sending}
-           placeholder="Your name"
+    <input
+      class="input input-bordered w-full"
+      id="name"
+      name="name"
+      type="text"
+      value={name}
+      required
+      disabled={sending}
+      placeholder="Your name"
     />
 
     <label class="label" for="email">
       <span class="label-text">Email</span>
     </label>
-    <input class="input input-bordered w-full"
-           id="email"
-           name="email"
-           type="email"
-           value={email}
-           required
-           disabled={sending}
-           placeholder="Email Address"
+    <input
+      class="input input-bordered w-full"
+      id="email"
+      name="email"
+      type="email"
+      value={email}
+      required
+      disabled={sending}
+      placeholder="Email Address"
     />
 
     <label class="label" for="reason">
       <span class="label-text">Reason for contacting</span>
     </label>
-    <select class="select select-bordered w-full"
-            name="reason"
-            id="reason"
-            disabled={sending}
-    >
+    <select class="select select-bordered w-full" name="reason" id="reason" disabled={sending}>
       <!--      <option disabled selected>Reason</option>-->
       {#key reason}
-        {#each ["Other", "Game idea", "Website issue"] as contactReason}
-          <option value="{contactReason}" selected={reason === contactReason}>{contactReason}</option>
+        {#each ['Other', 'Game idea', 'Website issue'] as contactReason}
+          <option value={contactReason} selected={reason === contactReason}>{contactReason}</option>
         {/each}
       {/key}
     </select>
@@ -97,19 +98,16 @@
     <label class="label" for="message">
       <span class="label-text">Message</span>
     </label>
-    <textarea class="textarea textarea-bordered textarea-lg w-full"
-              id="message"
-              name="message"
-              required
-              disabled={sending}
-              placeholder="Message"
-    >{message}</textarea>
+    <textarea
+      class="textarea textarea-bordered textarea-lg w-full"
+      id="message"
+      name="message"
+      required
+      disabled={sending}
+      placeholder="Message">{message}</textarea
+    >
   </div>
-  <button
-    disabled={sending}
-    type="submit"
-    class="btn float-right mt-4"
-  >
+  <button disabled={sending} type="submit" class="btn float-right mt-4">
     {#if sending}
       <span class="loading loading-spinner" />
       loading
