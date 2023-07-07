386fadefb543be850c3829dae5367c217c170566
zivoy <ziv132@gmail.com>
2023-07-07 02:12:27 -0400

    send webhook on message

--- website/src/routes/contact/+page.server.js
+++ website/src/routes/contact/+page.server.js
@@ -1,4 +1,5 @@
 import { fail } from '@sveltejs/kit';
+import { MESSAGES_WEBHOOK_URL } from '$env/static/private';
 
 /** @type {import("./$types").Actions} */
 export const actions = {
@@ -16,6 +17,12 @@ export const actions = {
         (formData.get('reason') ?? '').toString().trim()
       );
 
+      fetch(MESSAGES_WEBHOOK_URL, {
+        method: 'POST',
+        body: JSON.stringify(generateEmbed(record)),
+        headers: { 'content-type': 'application/json' }
+      });
+
       return {
         contactId: record.id
       };
@@ -32,24 +39,63 @@ export const actions = {
 };
 
 /**
+ * @typedef {import("../../dbtypes.d").Contact} Contact
  * @param {import("../../dbtypes.d").PocketBase} pb
  * @param {string} user
  * @param {string} name
  * @param {string} email
  * @param {string} message
  * @param {string} reason
- * @return {Promise<import("pocketbase").Record>}
+ * @return {Promise<Contact>}
  */
 async function sendContactMessage(pb, user, name, email, message, reason) {
   if (name === '') throw new Error('Name must be provided for contact');
   if (email === '') throw new Error('An email must be provided for contact');
   if (message === '') throw new Error('A message must be provided for contact');
 
-  return await pb.collection('contact').create({
-    name: name,
-    user: user,
-    reason: reason,
-    email: email,
-    message: message
-  });
+  return /** @type {Contact} */ (
+    await pb.collection('contact').create({
+      name: name,
+      user: user,
+      reason: reason,
+      email: email,
+      message: message
+    })
+  );
+}
+
+/**
+ * @param {Contact} contactMessage
+ * @return {Object.<string,*>}
+ */
+function generateEmbed(contactMessage) {
+  return {
+    content: null,
+    embeds: [
+      {
+        title: contactMessage.name,
+        color: 0x1e8cc2,
+        fields: [
+          {
+            name: 'Reason',
+            value: contactMessage.reason
+          },
+          {
+            name: 'Message',
+            value: '```\n' + contactMessage.message.replace('`', '\\`') + '\n```'
+          }
+        ],
+        author: {
+          name: contactMessage.email
+        },
+        footer: {
+          text: contactMessage.id
+        },
+        timestamp: new Date(contactMessage.created).toISOString()
+      }
+    ],
+    username: 'Contact message',
+    attachments: [],
+    flags: 4096
+  };
 }
