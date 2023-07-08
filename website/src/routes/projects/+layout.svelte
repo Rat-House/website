f8c3db2a87d5bc7d65ef13ad3ebef48905b09474
zivoy <ziv132@gmail.com>
2023-07-08 18:36:30 -0400

    small fixes

--- website/src/routes/projects/+layout.svelte
+++ website/src/routes/projects/+layout.svelte
@@ -1,12 +1,14 @@
 <noscript>
   <style>
-      #projectContent {
-          display: none;
-      }
+    #projectContent {
+      display: none;
+    }
   </style>
 
   <div class="grid place-content-center h-[70vh]">
-    <h1 class="text-lg sm:text-3xl xl:text-6xl font-bold text-error">This page requires JavaScript to work</h1>
+    <h1 class="text-lg sm:text-3xl xl:text-6xl font-bold text-error">
+      This page requires JavaScript to work
+    </h1>
   </div>
 </noscript>
 
