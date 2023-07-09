fd8b22ad98538572d4a46b2dad9f516930835b43
ikeia <ike.moore@student.ocadu.ca>
2023-07-09 17:50:12 -0400

    About Page Basic Layout

--- website/src/routes/about/+page.svelte
+++ website/src/routes/about/+page.svelte
@@ -1,1 +1,63 @@
-some about page with info and stuff
+<h1 class="text-6xl text-center mt-8 text-secondary font-bold">ABOUT US</h1>
+
+<div class="divider" />
+<div class="flex justify-center">
+  <p class="text-center px-12 py-8 max-w-4xl">
+    We are a passionate team of artists and developers who make games, websites and different kinds
+    of software solutions. If you would like to join the team or make any other inquiries please <a
+      href="contact"
+      class="text-accent font-bold hover:underline">contact us here</a
+    >
+  </p>
+</div>
+<div class="divider" />
+
+<div class="flex flex-col md:flex-row justify-center place-items-center">
+  <div class="flex flex-col md:flex-row place-items-center mx-8 my-2">
+    <a href="#">
+      <div class="avatar placeholder hover:opacity-50">
+        <div class="bg-neutral-focus text-neutral-content rounded-full w-36">
+          <span class="text-3xl text-center">Name</span>
+        </div>
+      </div>
+    </a>
+    <div class="text-center">
+      <p class="m-4 font-bold text-xl">Name:</p>
+      <p class="m-4">Job/Description</p>
+    </div>
+  </div>
+
+  <div class="divider md:divider-horizontal mx-16" />
+
+  <div class="flex flex-col md:flex-row place-items-center mx-8 my-2">
+    <a href="#">
+      <div class="avatar placeholder hover:opacity-50">
+        <div class="bg-neutral-focus text-neutral-content rounded-full w-36">
+          <span class="text-3xl">Name</span>
+        </div>
+      </div>
+    </a>
+    <div class="text-center">
+      <p class="m-4 font-bold text-xl">Name:</p>
+      <p class="m-4">Job/Description</p>
+    </div>
+  </div>
+
+  <div class="divider md:divider-horizontal mx-16" />
+
+  <div class="flex flex-col md:flex-row place-items-center mx-8 my-2">
+    <a href="#">
+      <div class="avatar placeholder hover:opacity-50">
+        <div class="bg-neutral-focus text-neutral-content rounded-full w-36">
+          <span class="text-3xl">Name</span>
+        </div>
+      </div>
+    </a>
+    <div class="text-center">
+      <p class="m-4 font-bold text-xl">Name:</p>
+      <p class="m-4">Job/Description</p>
+    </div>
+  </div>
+</div>
+
+<div class="md:divider" />
