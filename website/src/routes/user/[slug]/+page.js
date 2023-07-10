0c0d9c6abad778f8f7f4b8f785befb24afd15c70
zivoy <ziv132@gmail.com>
2023-07-09 23:38:11 -0400

    change to avatar

--- website/src/routes/user/[slug]/+page.js
+++ website/src/routes/user/[slug]/+page.js
@@ -1,6 +1,7 @@
 import { authFromCookie, pb } from '$lib/pocketbase.js';
 import { error } from '@sveltejs/kit';
 import { HeaderBuilder } from '$lib/headers.js';
+import { getAvatarUrl } from "$lib/tools.js";
 
 /**
  * @typedef {import("../../../dbtypes").User} User
@@ -31,6 +32,7 @@ export async function load({ parent, params }) {
         new HeaderBuilder()
           .setTitle(`${user.name}'s profile page`)
           .setDescription(`${user.name} is a ${auth.name}`)
+          .setImage(getAvatarUrl(user), `${user.name}'s avatar`)
           .save();
       })
       .catch(() => {
